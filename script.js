3eca5927307a360d2c410ba77063a0bcb34f3d30
Cristhian Castillo <97223412+Kal2403@users.noreply.github.com>
2023-05-15 18:11:07 +0200

    Fixing linter Error CSS and JS

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const open = document.querySelector('#open');
 const close = document.querySelector('#close');
 
 open.addEventListener('click', () => {
-    nav.classList.add('visible')
-})
+  nav.classList.add('visible');
+});
 
-close.addEventListener('click',() => {
-    nav.classList.remove('visible');
-})
+close.addEventListener('click', () => {
+  nav.classList.remove('visible');
+});
