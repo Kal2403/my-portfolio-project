f7d8166712202e966f7007b8fc567d14f332ad4c
Cristhian Castillo <97223412+Kal2403@users.noreply.github.com>
2023-05-15 17:16:20 +0200

    Added menu display function

---
+++ script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.nav');
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+
+open.addEventListener('click', () => {
+    nav.classList.add('visible')
+})
